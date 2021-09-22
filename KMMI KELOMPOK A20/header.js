b1dece63aa627d762edc0977abffb089450fb308
AsnanSabil <90853908+AsnanSabil@users.noreply.github.com>
2021-09-22 14:22:36 +0700

    Add files via upload

---
+++ KMMI KELOMPOK A20/header.js
@@ -0,0 +1,11 @@
+<script>
+function submit(x) {
+  x.style.height = "64px";
+  x.style.width = "64px";
+}
+
+function submit(x) {
+  x.style.height = "32px";
+  x.style.width = "32px";
+}
+</script>
